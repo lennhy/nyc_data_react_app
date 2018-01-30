9dda0eecdeb40553b6d9f716ee366a2c6553f591
lennhy <lennhydesign@gmail.com>
2018-01-30 16:34:43 -0500

    React Select Input box now works properly

--- src/App.js
+++ src/App.js
@@ -1,101 +1,57 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 require('dotenv').config()
 
-
-
-// let boro;
-// let housenumber;
-// let streetname;
-// // let startDate = "";
-// // let endDate = ""
-// let zip;
-// var url;
-
-// --------------------------- Output to display in the DOM
+// ---------- Start of component
 class App extends Component {
   constructor(props) {
-     super(props);
-     this.state = {
-       // name: [
-       //   "Manhattan",
-       //   "Queens",
-       //   "Brooklyn",
-       //   "Bronx",
-       //   "Staten Island"
-       // ],
-       boro: '',
-       housenumber: '',
-       streetname: '',
-       zip: '',
-       houses: []
-     };
-     this.handleChange = this.handleChange.bind(this);
-     this.handleSubmit = this.handleSubmit.bind(this);
+    super(props);
+    this.state = {
+      boro: '',
+      housenumber: '',
+      streetname: '',
+      zip: '',
+      houses: []
+    };
+    // --------- Bind the data to the state on every keydown
+    console.log(this)
+    this.handleChange = this.handleChange.bind(this);
+    // --------- Bind the data to the state when form is submitted
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // ----------- Handle input from form
+  handleChange(event) {
+    console.log(event.target.value)
+    const target = event.target;
+    const name = target.name;
+    this.setState({
+      [name]: target.value.toUpperCase()
+    });
 
   }
 
-   // Handle input from form
-   handleChange(event) {
-     const target = event.target;
-     const name = target.name;
-     this.setState({
-       [name]: target.value.toUpperCase()
-     });
-   }
-
-   handleSubmit(event) {
-     event.preventDefault();
-      alert('Your favorite flavor is: ' + this.state.housenumber + "," + this.state.streetname + "," + this.state.zip);
-   }
-
-  //  Rweturn data to DOM
- componentDidMount() {
-  var url;
-  if( this.state.housenumber && this.state.streetname && this.state.zip){
-    url = `https://data.cityofnewyork.us/resource/b2iz-pps8.json?housenumber=${this.state.housenumber}&streetname=${this.state.streetname}&zip=${this.state.zip}`;
+  // ----------- Handle submission of form
+  handleSubmit(event) {
+    event.preventDefault();
+    alert('Your favorite flavor is: ' + this.state.boro + this.state.housenumber + " , " + this.state.streetname + " , " + this.state.zip);
   }
-  // else{
-    url = "https://data.cityofnewyork.us/resource/b2iz-pps8.json?"
-  // }
-  axios.get(url, {
-    params: {
-      "$limit" : 10,
-      "$$app_token" : process.env.NYC_DATA_APP_TOKEN
-    }
-  })
-  .then(
-    (result) => {
-      this.setState({
-        isLoaded: true,
-        houses: result.data
-      });
-    },
-    (error) => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    }
-  )}
 
-  update() {
-    // console.log(prevState)
-    // if (this.state.zip !== prevState.zip) {
-      // var url;
-      // if( this.state.housenumber && this.state.streetname && this.state.zip){
-      var url = `https://data.cityofnewyork.us/resource/b2iz-pps8.json?boro=${this.state.boro}&housenumber=${this.state.housenumber}&streetname=${this.state.streetname}&zip=${this.state.zip}`;
-      // }
-      // else{
-      //   url = "https://data.cityofnewyork.us/resource/b2iz-pps8.json?"
-      // }
-      console.log(url);
-      axios.get(url, {
+  // ---------- Run the Socrata API
+  runApi() {
+    console.log(this.state);
+    var url;
+    if (this.state.boro && this.state.housenumber && this.state.streetname && this.state.zip) {
+      url = `https://data.cityofnewyork.us/resource/b2iz-pps8.json?boro=${this.state.boro}&housenumber=${this.state.housenumber}&streetname=${this.state.streetname}&zip=${this.state.zip}`;
+    } else {
+      url = "https://data.cityofnewyork.us/resource/b2iz-pps8.json?"
+    }
+    axios.get(url, {
         params: {
-          "$limit" : 10,
-          "$$app_token" : process.env.NYC_DATA_APP_TOKEN
+          "$limit": 10,
+          "$$app_token": process.env.NYC_DATA_APP_TOKEN
         }
       })
       .then(
@@ -113,11 +69,18 @@ class App extends Component {
           });
         }
       )
-    }
+  }
+  //  Return data to DOM
+  componentDidMount() {
+    this.runApi()
+  }
 
+  // Update the DOM with data from API after form submit values that change the parameters in the API url
+  update() {
+    this.runApi()
+  }
 
-    // console.log(this.state.houses)
-
+  // console.log(this.state.houses)
   render() {
     const { error, isLoaded, houses } = this.state;
     return (
@@ -147,8 +110,20 @@ class App extends Component {
         </form>
       */}
 
-      <form onSubmit={this.handleSubmit.bind(this)}>
+      <form onSubmit={this.handleSubmit}>
 
+      <label>
+        Choose a Borough:
+        <select value={this.state.value} name="boro" onChange={this.handleChange}>
+            <option value="manhattan">Manhattan</option>
+            <option value="bronx">Bronx</option>
+            <option value="brooklyn">Brooklyn</option>
+            <option value="queens">Queens</option>
+            <option value="staten_island">Staten Island</option>
+          </select>
+      </label>
+
+      {/* // Form for boros
       <label>
         Borough:
         <input type="text"
@@ -157,6 +132,7 @@ class App extends Component {
           onChange={this.handleChange}
         />
       </label>
+      */}
 
         <label>
           House:
@@ -192,7 +168,12 @@ class App extends Component {
         <ul>
           {houses.map((house, index )=> (
             <li key={index}>
-              {house.lot}
+              Address: {house.housenumber} {" "}
+              {house.streetname}
+              {house.apartment}, {" "}
+              {house.boro},
+              {house.zip} {" "}
+              {"("}{house.nta} {" area "}{")"}
             </li>
           ))}
         </ul>
@@ -203,6 +184,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-// $where=UPPER(field_name) = 'FOO'
