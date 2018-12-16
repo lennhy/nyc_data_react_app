95395ad0fbdb21f7e41fae5cee322928139d88a1
lennhy <lennhydesign@gmail.com>
2018-12-16 03:35:31 -0500

    Getting psycopg2.IntegrityError: duplicate key value violates unique constraint houses_pkey DETAIL:  Key (building_id)=(73852) already exists.

--- backend/run_data.py
+++ backend/run_data.py
@@ -19,7 +19,9 @@ client = Socrata("data.cityofnewyork.us",
 results = client.get("b2iz-pps8", limit=20)
 # print(results)
 df = pd.DataFrame(results)
-print (df)
+print("1st", df["housenumber"])
+# print("2nd",df.loc["housenumber"])
+
 try:
     conn = pysco.connect(dbname=os.environ['NYC_DATA_DB'], port='5432', user=os.environ['NYC_DATA_USER'], host=os.environ['NYC_DATA_ENDPOINT'], password='{}'.format(os.environ['MASTER_PASSWORD']))
     print("Connection Established")
@@ -29,11 +31,22 @@ except (Exception, pysco.DatabaseError) as error:
         print(error)
 
 cur = conn.cursor()
-
-cur.execute(
-    """INSERT INTO houses(building_id, violation_id, boro, house_number, street_name, zip, apartment, inspection_date, approved_date, current_status, current_status_date, violation_status, original_certify_by_date, community_board)
-    VALUES ('%d', '%d', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')""", (df['buildingid'].astype(int), df['violationid'], df['boro'], df['housenumber'], df['streetname'],df["zip"], df["apartment"], df["inspectiondate"],df["approveddate"],df["currentstatus"],df["currentstatusdate"],df["violationstatus"],df["originalcertifybydate"],df["communityboard"])
+# cur.close()
+    # WHERE violation_id NOT IN (
+     # SELECT houses.building_id
+     # FROM houses WHERE houses.building_id = %s %(df['boro']));
+for i, val in enumerate(df):
+    # print(len(df.length))
+    # if i < len(df.length):
+        # print(i)
+        # print(df[val][i])
+    # print("Here sir ", i)
+    # print("here ", i["housenumber"].values)
+    cur.execute(
+        """INSERT INTO houses(building_id, violation_id, boro, house_number, street_name, zip, apartment, inspection_date, approved_date, current_status, current_status_date, violation_status, original_certify_by_date, community_board)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (df['buildingid'][0], df['violationid'][0], df['boro'][0], df["housenumber"][0], df["streetname"][0],df["zip"][0], df["apartment"][0], df["inspectiondate"][0],df["approveddate"][0],df["currentstatus"][0],df["currentstatusdate"][0],df["violationstatus"][0],df["originalcertifybydate"][0],df["communityboard"][0])
     )
 
-
+conn.commit()
 cur.close()
+conn.close
