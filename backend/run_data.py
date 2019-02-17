169f69c5c4206cc8c7458cc06e14672ed57ea9ae
lennhy <lennhydesign@gmail.com>
2019-02-16 21:34:58 -0500

    Add more custom SoSoQL Clauses to make query to Socrata database more specific and to be able to page through database everytime cronjob runs to the following dataset based on the recent 2 years of activity.

--- backend/run_data.py
+++ backend/run_data.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sodapy import Socrata
 import psycopg2 as pysco
 import pandas.io.sql as pdsql
+import datetime
+
 
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
@@ -11,6 +13,15 @@ import pandas.io.sql as pdsql
 
 # Example authenticated client (needed for non-public datasets):
 # date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+# Current date
+today = datetime.datetime.now()
+# Date 2 years ago
+DD = datetime.timedelta(days=730)
+earlier = today - DD
+today = today.strftime("%Y-%m-%d")
+earlier_date = earlier.strftime("%Y-%m-%d")
+
 # url = "https://data.cityofnewyork.us/resource/b2iz-pps8.json?$where=approveddate between '2018-01-01T12:00:00.000' and '%s'" % date
 
 client = Socrata("data.cityofnewyork.us",
@@ -32,34 +43,25 @@ except (Exception, pysco.DatabaseError) as error:
 
 cur = conn.cursor()
 
-data = pdsql.read_sql_query("""select violation_id from houses limit 10;""", conn)
-# print(data)
+# data = pdsql.read_sql_query("""select violation_id from houses limit 1;""", conn)
+# data = pdsql.read_sql_query("""SELECT violation_id FROM houses order by violation_id desc limit 1 ;""", conn)
+data = pdsql.read_sql_query("""SELECT COUNT(*) FROM houses;""", conn)
+print(data)
 
-def getUniqueViolationId():
-    for i, row in data.iterrows():
-        results = client.get("b2iz-pps8", limit=2, order="approveddate DESC", where="violationid!=%s" % data['violation_id'][i])
-    # return results
+# SoQL Clauses
+# Oder by violationid start from the row that is equal to how many rows are in my database and continue in ascending order from where we left off and get data from 2 years back
+results = client.get('b2iz-pps8', limit=5 , order='violationid ASC', offset="{0}".format(data['count'][0]), where="approveddate between '{0}' and '{1}'".format(earlier_date, today))
+df = pd.DataFrame(results)
 
-    for f in results:
-        print(f['apartment'])
+
+for i, row in df.iterrows():
         cur.execute(
         """INSERT INTO houses(building_id, violation_id, boro, house_number, street_name, zip, apartment, inspection_date, approved_date, current_status, current_status_date, violation_status, community_board)
             SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-            WHERE NOT EXISTS (SELECT violation_id FROM houses WHERE houses.violation_id = %s )""",
-            (f['buildingid'], f['violationid'], f['boro'], f["housenumber"], f["streetname"], f["zip"], f["apartment"], f["inspectiondate"], f["approveddate"], f["currentstatus"], f["currentstatusdate"], f["violationstatus"], f["communityboard"], f['violationid'])
+            WHERE NOT EXISTS (SELECT violation_id FROM houses WHERE houses.violation_id = %s )""", # Make sure this violation id does not already exists in database
+            (df['buildingid'][i], df['violationid'][i], df['boro'][i], df["housenumber"][i], df["streetname"][i],df["zip"][i], df["apartment"][i], df["inspectiondate"][i],df["approveddate"][i],df["currentstatus"][i],df["currentstatusdate"][i],df["violationstatus"][i],df["communityboard"][i], df['violationid'][i])
         )
 
-getUniqueViolationId()
-
-# for i, row in df.iterrows():
-#     cur.execute(
-#     """INSERT INTO houses(building_id, violation_id, boro, house_number, street_name, zip, apartment, inspection_date, approved_date, current_status, current_status_date, violation_status, community_board)
-#         SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-#         WHERE NOT EXISTS (SELECT violation_id FROM houses WHERE houses.violation_id = %s )""",
-#         (df['buildingid'][i], df['violationid'][i], df['boro'][i], df["housenumber"][i], df["streetname"][i],df["zip"][i], df["apartment"][i], df["inspectiondate"][i],df["approveddate"][i],df["currentstatus"][i],df["currentstatusdate"][i],df["violationstatus"][i],df["communityboard"][i], df['violationid'][i])
-#     )
-data = pdsql.read_sql_query("""select * from houses;""", conn)
-# print(data)
 
 conn.commit()
 cur.close()
