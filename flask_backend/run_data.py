d35707b27b7867a1d5f184a7ddebf7f4042557fa
lennhy <lennhydesign@gmail.com>
2019-03-05 00:39:31 -0500

    Add flask backend template.

--- flask_backend/run_data.py
+++ flask_backend/run_data.py
@@ -50,19 +50,21 @@ print(data)
 
 # SoQL Clauses
 # Oder by violationid start from the row that is equal to how many rows are in my database and continue in ascending order from where we left off and get data from 2 years back
-results = client.get('b2iz-pps8', limit=5 , order='violationid ASC', offset="{0}".format(data['count'][0]), where="approveddate between '{0}' and '{1}'".format(earlier_date, today))
+results = client.get('b2iz-pps8', limit=2000 , order='violationid ASC', offset="{0}".format(data['count'][0]), where="approveddate between '{0}' and '{1}'".format(earlier_date, today))
 df = pd.DataFrame(results)
 
+def run_query():
+    for i, row in df.iterrows():
+            cur.execute(
+            """INSERT INTO houses(building_id, violation_id, boro, house_number, street_name, zip, apartment, inspection_date, approved_date, current_status, current_status_date, violation_status, community_board)
+                SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                WHERE NOT EXISTS (SELECT violation_id FROM houses WHERE houses.violation_id = %s )""", # Make sure this violation id does not already exists in database
+                (df['buildingid'][i], df['violationid'][i], df['boro'][i], df["housenumber"][i], df["streetname"][i],df["zip"][i], df["apartment"][i], df["inspectiondate"][i],df["approveddate"][i],df["currentstatus"][i],df["currentstatusdate"][i],df["violationstatus"][i],df["communityboard"][i], df['violationid'][i])
+            )
 
-for i, row in df.iterrows():
-        cur.execute(
-        """INSERT INTO houses(building_id, violation_id, boro, house_number, street_name, zip, apartment, inspection_date, approved_date, current_status, current_status_date, violation_status, community_board)
-            SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-            WHERE NOT EXISTS (SELECT violation_id FROM houses WHERE houses.violation_id = %s )""", # Make sure this violation id does not already exists in database
-            (df['buildingid'][i], df['violationid'][i], df['boro'][i], df["housenumber"][i], df["streetname"][i],df["zip"][i], df["apartment"][i], df["inspectiondate"][i],df["approveddate"][i],df["currentstatus"][i],df["currentstatusdate"][i],df["violationstatus"][i],df["communityboard"][i], df['violationid'][i])
-        )
 
-
-conn.commit()
-cur.close()
-conn.close()
+if __name__ == '__main__':
+    run_query()
+    conn.commit()
+    cur.close()
+    conn.close()
